59d35c4a6c0f767f3df5c47a28477327b0c610b1
Janan Akkad <jananakkad@hotmail.co.uk>
2023-07-16 17:40:10 +0100

    updated

--- js/index.js
+++ js/index.js
@@ -1,27 +1,42 @@
-const form = document.getElementById('github-form')
+const newForm = document.getElementById('github-form')
 
+newForm.addEventListener('submit', function (e){
+    e.preventDefault();
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault()
-    const search = document.getElementById('search').value
+    const search = document.getElementById("search").value
 
-    const userName = search.split(' ').join('')
+    return fetch('https://api.github.com/users/'+search)
+        .then (res => res.json())
+        .then (data => {
 
-    document.getElementById('user-list').innerHTML = ''
+            document.getElementById("github-container").innerHTML = `<img src="${data.avatar_url}"/>`
 
-    fetch('https://api.github.com/users/'+userName, {
-        headers: {
-            'Accept': 'application/vnd.github.v3+json'
-        }
-    })
-    .then((result) => result.json())
-    .then((data) => {
-        console.log(data)
+            const list = document.getElementById('github-container')
+            const li = document.createElement('li')
+            
+            li.innerHTML = `<a href="${data.html_url}" target="_blank">Link to profile!</a>
+            <br><br>
+            Username : ${data.login} 
+            <br><br> 
+            <button id='repoBtn'> Click to view this persons Repo's </button>
+            <br><br>
+            `
+            list.appendChild(li)
 
-        document.getElementById('user-list').innerHTML = 
-        `<a target="_blank" href="https://www.github.com/${userName}"><img src="${data.avatar_url}"/></a>`
-    })
-
+            
+            repoBtn.addEventListener('click', function(){
+                return fetch('https://api.github.com/users/AlexMinez/repos')
+                .then(res => res.json())
+                .then (data => {
+              
+                    console.log(data)
+                const repoList = document.getElementById('github-container')
+                const li2 = document.createElement('li')
+                        li2.innerHTML = `${data.full_name}`
+                        repoList.appendChild(li2)
+      })
+    })                                 
+  })     
 })
 
 //https://www.youtube.com/watch?v=7YeOjmpQUnU
